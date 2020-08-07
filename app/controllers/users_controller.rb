6e3b64aa134647eb70f12ab399ef5c91f51d1bab
Katsuya Minami <arms.mc.rp@gmail.com>
2020-08-08 00:51:35 +0900

    resourcesを使用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,10 @@ class UsersController < ApplicationController
     user.update(user_params)
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def destroy
     user = User.find(params[:id])
     user.destroy
